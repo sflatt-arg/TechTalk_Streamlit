47386913ba88be5ee1fe1957346f5b7dc9a15cd0
Solange Flatt <solange.flatt@argusa.ch>
2025-09-03 22:06:41 +0200

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import streamlit as st
 # Write directly to the app
 st.set_page_config(page_title="Publibike/Velospot")
 
-page_home = st.Page("pages/00_Home.py", title="Home")
-page_reservation = st.Page("pages/01_Reservation.py", title="Reservation")
-page_return = st.Page("pages/02_Return.py", title="Return")
-page_availability = st.Page("pages/03_Availability.py", title="Bike availability")
-page_map = st.Page("pages/04_Map.py", title="Map")
+page_home = st.Page("pages/00_home.py", title="Home")
+page_reservation = st.Page("pages/01_reservation.py", title="Reservation")
+page_return = st.Page("pages/02_return.py", title="Return")
+page_availability = st.Page("pages/03_availability.py", title="Bike availability")
+page_map = st.Page("pages/04_map.py", title="Map")
 
 pages = {
     "Home": [page_home],
